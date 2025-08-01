60fc6eea2a1e5867c541b7e412867355d1d14d86
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-01 08:08:22 +0200

    :sparkles: Get all issues paged

--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/issue/IssueController.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/issue/IssueController.java
@@ -2,10 +2,12 @@ package de.muenchen.issuepoker.issue;
 
 import de.muenchen.issuepoker.entities.Issue;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,11 @@ public class IssueController {
     @ResponseStatus(HttpStatus.OK)
     public Issue getIssue(@PathVariable("issueId") final long issueId) {
         return issueService.getIssue(issueId);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Issue> getIssuesPage(@RequestParam(defaultValue = "0") final int pageNumber, @RequestParam(defaultValue = "10") final int pageSize) {
+        return issueService.getAllIssues(pageNumber, pageSize);
     }
 }
--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/issue/IssueService.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/issue/IssueService.java
@@ -6,6 +6,9 @@ import de.muenchen.issuepoker.common.NotFoundException;
 import de.muenchen.issuepoker.entities.Issue;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +20,12 @@ public class IssueService {
     public Issue getIssue(final long issueId) {
         log.info("Get Issue with ID {}", issueId);
         return getIssueOrThrow(issueId);
+    }
+
+    public Page<Issue> getAllIssues(final int pageNumber, final int pageSize) {
+        log.info("Get all Issues at Page {} with a PageSize of {}", pageNumber, pageSize);
+        final Pageable pageRequest = PageRequest.of(pageNumber, pageSize);
+        return issueRepository.findAll(pageRequest);
     }
 
     private Issue getIssueOrThrow(final long issueId) {

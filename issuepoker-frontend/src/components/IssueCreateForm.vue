f7fe3190b4db481d61673c65b7cbc6377dfd4d0e
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-04 12:13:00 +0200

    :sparkles: Create UI for issue create form

---
+++ issuepoker-frontend/src/components/IssueCreateForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-form>
+    <v-row>
+      <v-text-field
+        v-model="title"
+        label="Titel"
+      ></v-text-field>
+    </v-row>
+    <v-row>
+      <v-textarea
+        v-model="description"
+        label="Beschreibung"
+      ></v-textarea>
+    </v-row>
+    <v-row align="center">
+      <v-col cols="auto">
+        <v-btn
+          :prepend-icon="mdiContentSave"
+          @click="save"
+          >Speichern
+        </v-btn>
+      </v-col>
+      <v-col cols="auto">
+        <v-btn
+          :prepend-icon="mdiCancel"
+          :to="{ name: ROUTES_ISSUES_LIST }"
+          >Abbrechen
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
+</template>
+
+<script lang="ts" setup>
+import { mdiCancel, mdiContentSave } from "@mdi/js";
+import { ref } from "vue";
+
+import { ROUTES_ISSUES_LIST } from "@/constants.ts";
+
+const title = ref("");
+const description = ref("");
+
+function save() {
+  description.value = title.value; // Dummy
+}
+</script>
--- issuepoker-frontend/src/views/IssueCreateView.vue
+++ issuepoker-frontend/src/views/IssueCreateView.vue
@@ -5,7 +5,14 @@
         <h1>Neues Issue Erstellen</h1>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <issue-create-form></issue-create-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import IssueCreateForm from "@/components/IssueCreateForm.vue";
+</script>

4c3b4c9d72eb113f842b2e7f9c815ba6437928b5
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-08 08:29:14 +0200

    :fire: Remove theentity

--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/security/Authorities.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/security/Authorities.java
@@ -8,16 +8,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
  * {@link org.springframework.stereotype.Service} classes in the method security annotations
  * (e.g. {@link PreAuthorize}).
  */
-@SuppressWarnings("PMD.DataClass")
 public final class Authorities {
     public static final String IS_USER = "hasAnyRole('user')";
     public static final String IS_ADMIN = "hasAnyRole('admin')";
-
-    public static final String THEENTITY_GET = IS_USER;
-    public static final String THEENTITY_GET_ALL = IS_USER;
-    public static final String THEENTITY_CREATE = IS_ADMIN;
-    public static final String THEENTITY_UPDATE = IS_ADMIN;
-    public static final String THEENTITY_DELETE = IS_ADMIN;
 
     private Authorities() {
         throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");

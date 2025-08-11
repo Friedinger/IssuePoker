1e88b32153d43f317407016754811c0471bd54c8
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-11 08:50:09 +0200

    :passport_control: Restrict access to create issue page

--- issuepoker-frontend/src/plugins/router.ts
+++ issuepoker-frontend/src/plugins/router.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import {
+  ROLE_ADMIN,
   ROUTES_HOME,
   ROUTES_ISSUE_CREATE,
   ROUTES_ISSUE_DETAIL,
 } from "@/constants";
+import { useUserStore } from "@/stores/user.ts";
 import HomeView from "@/views/HomeView.vue";
 import IssuesCreateView from "@/views/IssueCreateView.vue";
 import IssueDetailsView from "@/views/IssueDetailsView.vue";
@@ -25,6 +27,9 @@ const routes = [
     path: "/issues/new",
     name: ROUTES_ISSUE_CREATE,
     component: IssuesCreateView,
+    beforeEnter: () => {
+      return useUserStore().getUser?.authorities.includes(ROLE_ADMIN);
+    },
   },
   { path: "/:catchAll(.*)*", redirect: "/" }, // CatchAll route
 ];

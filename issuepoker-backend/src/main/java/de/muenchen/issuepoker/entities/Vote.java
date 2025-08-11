0ac041f3fc2e3718e7f53fd9b7dda1cffce7db60
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-11 14:56:34 +0200

    :recycle: Move votion options to global constant

--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/Vote.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/Vote.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import java.util.Set;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +14,8 @@ import lombok.Setter;
 @Getter
 @SuppressWarnings("PMD.ShortClassName")
 public class Vote {
+    public static final Set<Integer> VOTING_OPTIONS = Set.of(1, 2, 3, 5, 8, 13, 21);
+
     @GeneratedValue
     @Id
     private UUID id;
--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/validation/VoteValidator.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/validation/VoteValidator.java
@@ -1,19 +1,12 @@
 package de.muenchen.issuepoker.entities.validation;
 
+import de.muenchen.issuepoker.entities.Vote;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import java.util.Set;
 
 public class VoteValidator implements ConstraintValidator<ValidVote, Integer> {
-    private static final Set<Integer> ALLOWED_VALUES = Set.of(1, 2, 3, 5, 8, 13, 21);
-
-    @Override
-    public void initialize(ValidVote constraintAnnotation) {
-        // No initialization needed
-    }
-
     @Override
     public boolean isValid(final Integer value, ConstraintValidatorContext context) {
-        return value != null && ALLOWED_VALUES.contains(value);
+        return value != null && Vote.VOTING_OPTIONS.contains(value);
     }
 }

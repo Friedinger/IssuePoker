afc69bfe4908206641d221ef921b0ee623333722
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-11 15:16:46 +0200

    :bug: Use tree set so that voting options are in intended order

--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/Vote.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/Vote.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +15,7 @@ import lombok.Setter;
 @Getter
 @SuppressWarnings("PMD.ShortClassName")
 public class Vote {
-    public static final Set<Integer> VOTING_OPTIONS = Set.of(1, 2, 3, 5, 8, 13, 21);
+    public static final Set<Integer> VOTING_OPTIONS = new TreeSet<>(Set.of(1, 2, 3, 5, 8, 13, 21));
 
     @GeneratedValue
     @Id

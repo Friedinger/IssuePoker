2fcc7f593215fc52ec1e32564000bb3ab452bcce
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-18 16:16:11 +0200

    :sparkles: Display repository info in details view

--- issuepoker-frontend/src/types/IssueDetails.ts
+++ issuepoker-frontend/src/types/IssueDetails.ts
@@ -1,5 +1,7 @@
 export default interface IssueDetails {
   id: number;
+  owner: string;
+  repository: string;
   title: string;
   description: string;
 }
--- issuepoker-frontend/src/views/IssueDetailsView.vue
+++ issuepoker-frontend/src/views/IssueDetailsView.vue
@@ -2,24 +2,27 @@
   <v-container>
     <v-row align="center">
       <v-col>
-        <h1 v-if="issue">{{ issue.title }}</h1>
-        <h1 v-else>Issue wurde nicht gefunden</h1>
+        <template v-if="issue">
+          <h1>{{ issue.title }}</h1>
+          <p>{{ issue.owner }}/{{ issue.repository }} #{{ issue.id }}</p>
+        </template>
       </v-col>
       <v-col cols="auto">
         <v-btn :to="{ name: ROUTES_HOME }">Zurück zur Liste</v-btn>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col>
-        <p v-if="issue">{{ issue.description }}</p>
-        <p v-else>Bitte zurück zur Liste gehen.</p>
-      </v-col>
-    </v-row>
-    <v-row v-if="issue">
-      <v-col>
-        <issue-voting :issue="issue" />
-      </v-col>
-    </v-row>
+    <template v-if="issue">
+      <v-row>
+        <v-col>
+          <p>{{ issue.description }}</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <issue-voting :issue="issue" />
+        </v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 
@@ -32,8 +35,8 @@ import { useRoute } from "vue-router";
 import { getIssue } from "@/api/fetch-issue.ts";
 import IssueVoting from "@/components/IssueVoting.vue";
 import { ROUTES_HOME, STATUS_INDICATORS } from "@/constants.ts";
-import router from "@/plugins/router.ts";
 import { useSnackbarStore } from "@/stores/snackbar.ts";
+import router from "@/plugins/router.ts";
 
 const snackbarStore = useSnackbarStore();
 const route = useRoute();

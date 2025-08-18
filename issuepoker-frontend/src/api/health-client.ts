477513c6233bc87f9357f6b64f371ff754eaec97
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-18 14:30:28 +0200

    :lipstick: Display user and status information in app bar

--- issuepoker-frontend/src/api/health-client.ts
+++ issuepoker-frontend/src/api/health-client.ts
@@ -1,8 +1,19 @@
 import { defaultResponseHandler, getConfig } from "@/api/fetch-utils";
 import HealthState from "@/types/HealthState";
 
-export function checkHealth(): Promise<HealthState> {
+export function checkGatewayHealth(): Promise<HealthState> {
   return fetch("actuator/health", getConfig())
+    .then((response) => {
+      defaultResponseHandler(response);
+      return response.json();
+    })
+    .catch((err) => {
+      defaultResponseHandler(err);
+    });
+}
+
+export function checkBackendHealth(): Promise<HealthState> {
+  return fetch("api/backend-service/actuator/health", getConfig())
     .then((response) => {
       defaultResponseHandler(response);
       return response.json();

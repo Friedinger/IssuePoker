d77a52f6081bab7becfc2108162081b3ff8eb837
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-08-21 11:44:24 +0200

    :rotating_light: Fix pmd warnings

--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/IssueKey.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/IssueKey.java
@@ -1,8 +1,10 @@
 package de.muenchen.issuepoker.entities;
 
 public record IssueKey(String owner, String repository, long number) {
-    public static String sanitizeForLog(String input) {
-        if (input == null) return "";
+    public static String sanitizeForLog(final String input) {
+        if (input == null) {
+            return "";
+        }
         return input.replaceAll("[\\r\\n\\t]", "_")
                 .replaceAll("[\\x00-\\x1F\\x7F]", "_");
     }

a160b8d55becce8a3b8353da4bcd91ddf0aa8ca3
Friedinger <71267611+Friedinger@users.noreply.github.com>
2025-09-04 08:28:00 +0200

    :recycle: Move log sanitize to utils

--- issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/issue/IssueKey.java
+++ issuepoker-backend/src/main/java/de/muenchen/issuepoker/entities/issue/IssueKey.java
@@ -1,14 +1,8 @@
 package de.muenchen.issuepoker.entities.issue;
 
-public record IssueKey(String owner, String repository, long number) {
-    public static String sanitizeForLog(final String input) {
-        if (input == null) {
-            return "";
-        }
-        return input.replaceAll("[\\r\\n\\t]", "_")
-                .replaceAll("[\\x00-\\x1F\\x7F]", "_");
-    }
+import static de.muenchen.issuepoker.util.LogUtil.sanitizeForLog;
 
+public record IssueKey(String owner, String repository, long number) {
     @Override
     public String toString() {
         return sanitizeForLog(owner + "/" + repository + "#" + number);
